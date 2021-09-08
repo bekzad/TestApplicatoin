a42e19feba50b564ed96b333dd129d5332c47b49
Бекзадбек Насирахунов <n.bekzad@gmail.com>
2021-09-08 09:33:06 +0600

    Initial commit

---
+++ app/src/main/java/com/bekzad/testapplicatoin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bekzad.testapplicatoin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bekzad.testapplicatoin.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/bekzad/testapplicatoin/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.bekzad.testapplicatoin.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
